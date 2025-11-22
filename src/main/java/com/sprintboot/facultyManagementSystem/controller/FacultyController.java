8f4658930c21f482eb3006178964e2e4e81a9416
Krishanu Saha <krishanu1137@gmail.com>
2025-11-22 15:58:24 +0530

    allowed cors

--- src/main/java/com/sprintboot/facultyManagementSystem/controller/FacultyController.java
+++ src/main/java/com/sprintboot/facultyManagementSystem/controller/FacultyController.java
@@ -8,9 +8,10 @@ import com.sprintboot.facultyManagementSystem.model.Employee;
 import com.sprintboot.facultyManagementSystem.service.FacultyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class FacultyController {
